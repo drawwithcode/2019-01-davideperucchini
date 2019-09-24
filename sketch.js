6c0ba21ea992a09873ed5f7abae88ad254b87924
davideperucchini <37082014+davideperucchini@users.noreply.github.com>
2019-09-24 09:42:03 +0200

    change color

--- sketch.js
+++ sketch.js
@@ -13,14 +13,15 @@ function draw() {
 
   stroke(
     lerpColor(
-      color("aquamarine"),
-      color("tomato"),
+      color("#fae3d9"),
+      color("#8ac6d1"),
     frameCount/120)
   );
 
-  let k=width/5;
+  let k=width/7;
   translate(width/2,height/2);
   line(k,0,k*cos(frameCount*3),k*sin(frameCount*3));
   if(frameCount == 120)
   noLoop();
+
 }
